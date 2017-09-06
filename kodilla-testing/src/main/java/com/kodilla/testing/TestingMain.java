9422b9caa6962619141b7736d8a5ae3782a17735
BartekOgien <bartekogien2106@wp.pl>
2017-09-06 18:55:34 +0200

    Task 6.2 kodilla course

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,34 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+import com.kodilla.testing.user.SimpleUser;
+
 public class TestingMain {
     public static void main(String[] args){
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+
+        String result = simpleUser.getUsername();
+
+        if (result.equals("theForumUser")){
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
+
+        //Calculator test
+        Calculator calculator = new Calculator();
+        if (calculator.add(3, 5)==8) {
+            System.out.println("First calculator test ok");
+        }
+        else {
+            System.out.println("Error");
+        }
+
+        if (calculator.substract(7, 5)==2) {
+            System.out.println("Second calculator test ok");
+        }
+        else {
+            System.out.println("Error");
+        }
     }
 }
