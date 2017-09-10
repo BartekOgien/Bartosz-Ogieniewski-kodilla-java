b4133e300bb357c82f2a44947319bb1839496d21
BartekOgien <bartekogien2106@wp.pl>
2017-09-10 09:55:21 +0200

    Task 6.4 corrected

--- kodilla-testing/src/main/java/com/kodilla/testing/shape/Circle.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/shape/Circle.java
@@ -3,7 +3,6 @@ package com.kodilla.testing.shape;
 public class Circle implements Shape {
     private final String name = "Circle";
     private double field;
-    private double radius;
 
     public Circle(double field) {
         this.field = field;
@@ -17,5 +16,24 @@ public class Circle implements Shape {
         return field;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
 
+        Circle circle = (Circle) o;
+
+        if (Double.compare(circle.field, field) != 0) return false;
+        return name != null ? name.equals(circle.name) : circle.name == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result;
+        long temp;
+        result = name != null ? name.hashCode() : 0;
+        temp = Double.doubleToLongBits(field);
+        result = 31 * result + (int) (temp ^ (temp >>> 32));
+        return result;
+    }
 }
--- kodilla-testing/src/main/java/com/kodilla/testing/shape/ShapeCollector.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/shape/ShapeCollector.java
@@ -11,19 +11,10 @@ public class ShapeCollector {
     }
 
     public boolean removeFigure(Shape shape) {
-        boolean result = false;
-        if(figureList.contains(shape)) {
-            figureList.remove(shape);
-            result = true;
-        }
-        return result;
+        return figureList.remove(shape);
     }
 
     public Shape getFigure(int n) {
-        Shape figure = null;
-        if(n>=0 && n<=figureList.size()-1) {
-            figure = figureList.get(n);
-        }
-        return figure;
+        return figureList.get(n);
     }
 }
