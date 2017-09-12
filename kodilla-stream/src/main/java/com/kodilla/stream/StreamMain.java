22dc2852a812033f26144c424f76463a920532c9
BartekOgien <bartekogien2106@wp.pl>
2017-09-12 19:22:27 +0200

    Task 7.1

---
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -0,0 +1,19 @@
+package com.kodilla.stream;
+import com.kodilla.stream.beautifier.PoemBeautifier;
+
+public class StreamMain {
+    public static void main(String[] args) {
+
+        PoemBeautifier poemBeautifier = new PoemBeautifier();
+        poemBeautifier.beautify("Kodilla", (text) -> "|||" + text + "|||");
+        poemBeautifier.beautify("Kodilla", (text) -> "ABC " + text + " CBA");
+        poemBeautifier.beautify("Kodilla", (text) -> "##" + text.toUpperCase() + "##");
+        poemBeautifier.beautify("Kodilla", (text) -> {
+                                                            String result = "";
+                                                            for (int i=0; i<text.length(); i++) {
+                                                                 result = result + text.charAt(i) + "   ";
+                                                            }
+                                                            return "| || |||   " + result + "||| || |";
+                                                            });
+    }
+}
