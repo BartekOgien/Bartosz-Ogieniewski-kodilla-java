932cd0e1e5e1b86e70cc140d371c2e96a8ce0ad2
BartekOgien <bartekogien2106@wp.pl>
2017-09-14 18:22:53 +0200

    Task 7.3 completed

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,25 +1,23 @@
 package com.kodilla.stream;
-import com.kodilla.stream.beautifier.PoemBeautifier;
-import com.kodilla.stream.iterate.NumbersGenerator;
+
+import com.kodilla.stream.forumuser.Forum;
+import com.kodilla.stream.forumuser.ForumUser;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class StreamMain {
     public static void main(String[] args) {
 
-        PoemBeautifier poemBeautifier = new PoemBeautifier();
-        poemBeautifier.beautify("Kodilla", (text) -> "|||" + text + "|||");
-        poemBeautifier.beautify("Kodilla", (text) -> "ABC " + text + " CBA");
-        poemBeautifier.beautify("Kodilla", (text) -> "##" + text.toUpperCase() + "##");
-        poemBeautifier.beautify("Kodilla", (text) -> {
-                                                            String result = "";
-                                                            for (int i=0; i<text.length(); i++) {
-                                                                 result = result + text.charAt(i) + "   ";
-                                                            }
-                                                            return "| || |||   " + result + "||| || |";
-                                                            });
+        Forum forum = new Forum();
+        Map<Integer, ForumUser> resultMapOfUsers = forum.getUserList().stream()
+                                                                     .filter(f -> f.getSex()=='M')
+                                                                      .filter(f -> f.getDateOfBirth().getYear()<1997)
+                                                                      .filter(f -> f.getNumberOfPosts()>0)
+                                                                        .collect(Collectors.toMap(f -> f.getUserID(), f -> f));
 
-        System.out.println("Using Stream to generate even numbers from 1 to 20");
-        NumbersGenerator.generateEven(20);
+        resultMapOfUsers.entrySet().stream()
+                                     .map(e -> "User ID: " + e.getKey() + " // "+e.getValue())
+                                     .forEach(System.out::println);
+
     }
-
-
 }
