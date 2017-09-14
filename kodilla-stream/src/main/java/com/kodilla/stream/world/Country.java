aad82886a2984a8dedf182e34070b028aa63c7ce
BartekOgien <bartekogien2106@wp.pl>
2017-09-14 19:23:18 +0200

    Task 7.4 completed

---
+++ kodilla-stream/src/main/java/com/kodilla/stream/world/Country.java
@@ -0,0 +1,17 @@
+package com.kodilla.stream.world;
+
+import java.math.BigDecimal;
+
+public final class Country {
+    private final String nameOfCountry;
+    private final BigDecimal peopleQuantity;
+
+    public Country(final String nameOfCountry,final  BigDecimal peopleQuantity) {
+        this.nameOfCountry = nameOfCountry;
+        this.peopleQuantity = peopleQuantity;
+    }
+
+    public BigDecimal getPeopleQuantity(){
+        return peopleQuantity;
+    }
+}
