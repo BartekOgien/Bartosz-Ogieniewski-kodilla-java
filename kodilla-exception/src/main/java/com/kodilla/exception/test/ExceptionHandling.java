2eea6e79ff9a0db98a8be92cae73969a93baf43f
BartekOgien <bartekogien2106@wp.pl>
2017-09-20 20:09:00 +0200

    Task 8.3

---
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/ExceptionHandling.java
@@ -0,0 +1,15 @@
+package com.kodilla.exception.test;
+
+public class ExceptionHandling {
+
+    public static void main(String[] args) {
+
+        SecondChallenge secondChallenge = new SecondChallenge();
+        try {
+            secondChallenge.probablyIWillThrowException(0, 3);
+
+        } catch (Exception e) {
+            System.out.println("Wrong arguments. Y must be not equals 1,5 and X must be lesser than 2 and greater/equals than 1");
+        }
+    }
+}
