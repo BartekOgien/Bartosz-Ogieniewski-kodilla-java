dede710a5dbea272f1c51234b15b9d6a34da2665
BartekOgien <bartekogien2106@wp.pl>
2017-10-29 13:34:44 +0100

    Task 13.4

--- kodilla-hibernate/src/main/java/com/kodilla/hibernate/manytomany/Company.java
+++ kodilla-hibernate/src/main/java/com/kodilla/hibernate/manytomany/Company.java
@@ -5,6 +5,10 @@ import javax.validation.constraints.NotNull;
 import java.util.ArrayList;
 import java.util.List;
 
+@NamedNativeQuery(name = "Company.retrieveCompanyNameBySubstring",
+        query = "SELECT * FROM COMPANIES WHERE COMPANY_NAME LIKE CONCAT('', :SUBSTRING, '%')",
+        resultClass = Company.class)
+
 @Entity
 @Table(name = "COMPANIES")
 public class Company {
