d28c4ce6956bc84b4ca718d6c523270a5fda34f6
BartekOgien <bartekogien2106@wp.pl>
2017-12-19 18:41:11 +0100

    Task 20.1 corrected

--- kodilla-hibernate/src/main/java/com/kodilla/hibernate/manytomany/facade/Facade.java
+++ kodilla-hibernate/src/main/java/com/kodilla/hibernate/manytomany/facade/Facade.java
@@ -4,33 +4,33 @@ import com.kodilla.hibernate.manytomany.Company;
 import com.kodilla.hibernate.manytomany.Employee;
 import com.kodilla.hibernate.manytomany.dao.CompanyDao;
 import com.kodilla.hibernate.manytomany.dao.EmployeeDao;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
 
+
 @Service
+@Slf4j
 public final class Facade {
     @Autowired
     CompanyDao companyDao;
     @Autowired
     EmployeeDao employeeDao;
-    private static final Logger LOGGER = LoggerFactory.getLogger(Facade.class);
 
     public List<Company> findCompanyBySubstring(final String substring) {
         List<Company> resultCompanies = companyDao.retrieveCompanyNameByArg(substring);
         if(resultCompanies.size() == 0) {
-            LOGGER.warn("No companies found where name contains: " + substring);
-            return new ArrayList<>();
+            log.warn("No companies found where name contains: " + substring);
+            return resultCompanies;
         }
         else {
-            LOGGER.info("Found companies: ");
+            log.info("Found companies: ");
             resultCompanies.stream()
                     .map(company -> company.getName())
-                    .forEach(companyname -> LOGGER.info(companyname));
+                    .forEach(companyname -> log.info(companyname));
             return resultCompanies;
         }
     }
@@ -38,14 +38,14 @@ public final class Facade {
     public List<Employee> findEmployeesBySubstring(final String substring) {
         List<Employee> employeeList = employeeDao.retrieveEmployersBySubstring(substring);
         if(employeeList.size() == 0) {
-            LOGGER.warn("No employees found where lastname contains: " + substring);
+            log.warn("No employees found where lastname contains: " + substring);
             return new ArrayList<>();
         }
         else {
-            LOGGER.info("Found employees: ");
+            log.info("Found employees: ");
             employeeList.stream()
                     .map(e -> e.getLastname())
-                    .forEach(e -> LOGGER.info(e));
+                    .forEach(e -> log.info(e));
             return employeeList;
         }
     }
